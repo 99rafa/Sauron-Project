d238be5dbb38b1dd6e7048dcb5b2fb329cbbc88d
Rafael Alexandre <r.alexandre99@gmail.com>
2020-03-29 23:30:49 +0100

    service methods join and info

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImp.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImp.java
@@ -1,4 +1,36 @@
 package pt.tecnico.sauron.silo;
 
+import io.grpc.stub.StreamObserver;
+import pt.tecnico.sauron.silo.domain.Silo;
+import pt.tecnico.sauron.silo.grpc.CamInfoRequest;
+import pt.tecnico.sauron.silo.grpc.CamInfoResponse;
+import pt.tecnico.sauron.silo.grpc.CamJoinRequest;
+import pt.tecnico.sauron.silo.grpc.CamJoinResponse;
+
 public class SiloServiceImp {
+
+    private Silo silo = new Silo();
+
+    @Override
+    public void joinCameraToSilo(CamJoinRequest request, StreamObserver<CamJoinResponse> responseObserver) {
+
+        CamJoinResponse response = CamJoinResponse.newBuilder().build(); //TO DO
+
+        // Send a single response through the stream.
+        responseObserver.onNext(response);
+        // Notify the client that the operation has been completed.
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getCameraInfo(CamInfoRequest request, StreamObserver<CamInfoResponse> responseObserver) {
+
+
+        CamInfoResponse response = CamInfoResponse.newBuilder().build(); //TO DO
+
+        // Send a single response through the stream.
+        responseObserver.onNext(response);
+        // Notify the client that the operation has been completed.
+        responseObserver.onCompleted();
+    }
 }
