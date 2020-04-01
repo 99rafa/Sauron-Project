0fee8b727991a7a7c1931edcf32fec8423176913
PedroNunes99 <pedro.m.s.nunes@tecnico.ulisboa.pt>
2020-04-01 18:11:17 +0100

    SpotterApp fixed

--- silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImp.java
+++ silo-server/src/main/java/pt/tecnico/sauron/silo/SiloServiceImp.java
@@ -86,8 +86,7 @@ public class SiloServiceImp extends SiloOperationsServiceGrpc.SiloOperationsServ
             Observation result;
 
 
-            if (type == Type.UNRECOGNIZED)
-                throw new SiloException(ErrorMessage.OBJECT_INVALID_TYPE, type.toString());
+
 
             result = silo.trackObject(type, id);
 
@@ -111,7 +110,8 @@ public class SiloServiceImp extends SiloOperationsServiceGrpc.SiloOperationsServ
 
         } catch (SiloException e) {
             if(e.getErrorMessage() == ErrorMessage.OBSERVATION_NULL_ID
-                || e.getErrorMessage() == ErrorMessage.OBJECT_NULL_TYPE)
+                || e.getErrorMessage() == ErrorMessage.OBJECT_NULL_TYPE
+                    || e.getErrorMessage() == ErrorMessage.OBJECT_INVALID_TYPE)
                 responseObserver.onError(INVALID_ARGUMENT.withDescription(e.getMessage()).asRuntimeException());
             if(e.getErrorMessage() == ErrorMessage.NO_SUCH_OBJECT)
                 responseObserver.onError(NOT_FOUND.withDescription(e.getMessage()).asRuntimeException());
@@ -129,8 +129,8 @@ public class SiloServiceImp extends SiloOperationsServiceGrpc.SiloOperationsServ
             List<Observation> result;
             TrackMatchResponse.Builder builder = TrackMatchResponse.newBuilder();
 
-            if (type == Type.UNRECOGNIZED)
-                throw new SiloException(ErrorMessage.OBJECT_INVALID_TYPE, type.toString());
+            if (type == Type.UNKNOWN)
+                throw new SiloException(ErrorMessage.OBJECT_INVALID_TYPE);
 
             result = silo.trackMatchObject(type, id);
 
@@ -177,8 +177,8 @@ public class SiloServiceImp extends SiloOperationsServiceGrpc.SiloOperationsServ
             String id = request.getId();
             List<Observation> result;
             TraceResponse.Builder builder = TraceResponse.newBuilder();
-            if (type == Type.UNRECOGNIZED)
-                throw new SiloException(ErrorMessage.OBJECT_INVALID_TYPE, type.toString());
+            if (type == Type.UNKNOWN)
+                throw new SiloException(ErrorMessage.OBJECT_INVALID_TYPE);
 
             result = silo.traceObject(type, id);
 
